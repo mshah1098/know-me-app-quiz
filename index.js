b55f287c66219146bd829ef214b42fd33f09fac5
Meet Shah <109501983+mtshah92@users.noreply.github.com>
2022-10-04 16:55:48 +0000

    added questions

--- index.js
+++ index.js
@@ -11,16 +11,22 @@ function qna(question, answer) {
   var userAnswer = readlineSync.question(question);
   var result = userAnswer.toLocaleLowerCase()
 
-  if (userAnswer === result) {
+  if (result === answer) {
     console.log("Right")
     score = score + 1;
+  }
+  else {
+    console.log("No, The correct answer is " + answer);
   }
 
 
 }
 
-qna("Where the meet live? ", "HMT");
-qna("Where the meet work? ", "Wipro");
+qna("Where the meet live? ", "hmt");
+qna("Where the meet work? ", "wip");
+qna("Wich his favourite food item? ", "pizza");
+qna("Wich his favourite game? ", "cricket");
+qna("Wich his favourite movie? ", "Sholay");
 
 console.log("Your final score is ", score);
 
